9e0433d8dbc42f485d4484df4b4d35d18aaf1eb6
rsabbah <rpasabb@gmail.com>
2025-02-07 10:09:22 +0100

    install script

---
+++ p1/scripts/install-dependencies.sh
@@ -0,0 +1,30 @@
+# Install required dependencies
+
+# Update package list
+apt-get updatek
+
+# Install required packages
+apt-get install -y curl
+
+# Install k3s
+SERVER_URL="https://$1:6443"
+NETINT="enp0s8"
+HOSTNAME="$(hostname)"
+TOKEN_FILE="/home/vagrant/shared/token"
+
+if [ ! -f /etc/rancher/k3s/k3s.yaml ]; then
+	if [[ "$HOSTNAME" == "rsabbahS" ]]; then
+		curl -sfL https://get.k3s.io | sh -s - server \
+			--node-ip $1 \
+			--flannel-iface "$NETINT" 
+		cp /var/lib/rancher/k3s/server/node-token "$TOKEN_FILE"
+	else
+		while [ ! -f "$TOKEN_FILE" ]; do
+			sleep 2
+		done
+		curl -sfL https://get.k3s.io | sh -s - agent \
+			--server "$SERVER_URL" \
+			--token-file "$TOKEN_FILE" \
+			--flannel-iface "$NETINT"
+	fi
+fi
